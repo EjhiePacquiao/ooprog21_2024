b0ff62af9186e0ee266c9dda1fb75602300949d9
EjhiePacquiao <pacquiaoejhie@gmail.com>
2024-08-14 05:40:00 -0800

    MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class AugustRushQuoteInfo
+{
+    public static void main(String args[])
+    {
+        System.out.println("Quote: \"The Music is all around us, All you have to do is Listen.\"");
+        System.out.println("Show: August Rush");
+        System.out.println("Character: Evan Taylor aka( August Rush )");
+        System.out.println("Year: 2007");
+    }
+}
