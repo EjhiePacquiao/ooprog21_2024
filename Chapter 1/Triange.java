2aff83233d37c42f033f80409be5817d7fe2636e
EjhiePacquiao <pacquiaoejhie@gmail.com>
2024-08-16 08:25:48 +0800

    Triange.java

--- Chapter 1/Triange.java
+++ Chapter 1/Triange.java
@@ -3,17 +3,17 @@ public class Traingle
    public static void main(String args[])
    {
    
-      for(int i = 1; i <= 5; i++)//PARA SPACE
+      for(int i = 1; i <= 7; i++)//PARA SPACE for loop
       {
-         for(int s = 1; s <= 5 - i; s++)
+         for(int s = 1; s <= 7 - i; s++)
          {  //s = space
-            //s = 5-i
+            //s = 7-i
             System.out.print(" ");
          }  
       
-         for(int j = 1; j <= 2 * i - 1; j++)// PARA ASTERISK
+         for(int j = 1; j <= 2 * i - 1; j++)// PARA T for loop
          {  //* = 2*i-1
-            System.out.print("*");
+            System.out.print("T");
          }
          
          System.out.println();
