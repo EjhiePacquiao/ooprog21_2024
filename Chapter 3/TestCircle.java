72d70b1936de8a4b9992ffdc29f8c7d09f9ec48f
EjhiePacquiao <pacquiaoejhie@gmail.com>
2024-10-04 09:45:20 +0800

    Add files via upload

---
+++ Chapter 3/TestCircle.java
@@ -0,0 +1,24 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        
+        Circle a = new Circle();
+        Circle b = new Circle();
+        Circle c = new Circle();
+        
+        a.setRadius(3);
+        System.out.println("Radius of the first circle: " + a.getRadius());
+        System.out.println("Diameter of the first circle: " + a.getDiameter());
+        System.out.println("Area of the first circle: " + a.getArea());
+        
+        
+        b.setRadius(20);
+        System.out.println("\nRadius of the second circle: " + b.getRadius());
+        System.out.println("Diameter of the second circle: " + b.getDiameter());
+        System.out.println("Area of the second circle: " + b.getArea());
+        
+        c.setRadius(35);
+        System.out.println("\nRadius of the third circle: " + c.getRadius());
+        System.out.println("Diameter of the second circle: " + c.getDiameter());
+        System.out.println("Area of the second circle: " + c.getArea());
+    }
+}
